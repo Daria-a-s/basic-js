0ddd94d06c465c0c9843a23f9f456ced41f1a460
Daria Sukhova <d.es.tiny.1234545@gmail.com>
2020-10-07 23:12:41 +0300

    feat: add "transform-array" solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) return THROWN;
+  let res_arr = [];
+  let arr_length = arr.length;
+  let i = 0;
+  if ((arr[0] == '--discard-prev' ) || (arr[0] == '--double-prev')) i++;
+  if ((arr[arr_length-1] == '--discard-next') || (arr[arr_length-1] == '--double-next')) arr_length--;
+  while ( i < arr.length)
+  {
+    if (Number.isInteger(arr[i]))
+    {
+      res_arr.push(arr[i]);
+      i++;
+    } 
+    else
+    switch (arr[i])
+      {
+        case '--discard-next':
+          i+=2;
+          if ((arr[i] == '--discard-prev') || (arr[i] == '--double-prev')) i++;
+          break;
+        
+        case '--discard-prev':
+            res_arr.splice(res_arr.length-1,1);
+            i++;
+          break;
+
+          case '--double-next':
+            {
+              i++;
+              res_arr.push(arr[i]);
+            }
+            break;
+
+          case '--double-prev':
+              res_arr.push(arr[i-1]);
+              i++;
+            break;
+      }
+  }
+  return res_arr;
 };
